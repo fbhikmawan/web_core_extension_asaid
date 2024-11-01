e01fd3bb03f51e4b2b71f667f9ee43b1365c7653
Fatchul Bari Hikmawan <fb.hikmawan@gmail.com>
2024-11-02 01:32:34 +0700

    initial version for Odoo 18.0

--- __manifest__.py
+++ __manifest__.py
@@ -17,7 +17,7 @@
     # Check https://github.com/odoo/odoo/blob/17.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
     'category': 'Website',
-    'version': '17.0.1.0.0',
+    'version': '18.0.1.0.0',
 
     # any module necessary for this one to work correctly
     'depends': ['base', 'web'],
